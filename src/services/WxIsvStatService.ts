a685a81cd829c6290c7e99f9c4b3d69ab3d2b1c3
Will <willerhe@126.com>
2021-05-12 11:06:54 +0800

    统计接口添加错误忽略

--- src/services/WxIsvStatService.ts
+++ src/services/WxIsvStatService.ts
@@ -4,28 +4,28 @@ import { WxIsv } from '../typings'
 export class WxIsvStatService extends WxIsvServiceBase {
 
   // 获取用户访问小程序每日数据概况
-  async getDailySummary (accessToken: string, begin_date: string, end_date: string) {
-    return await this.request('POST', '/datacube/getweanalysisappiddailysummarytrend', { begin_date, end_date }, { access_token: accessToken }) as WxIsv.WxIsvStatDailySummary
+  async getDailySummary (accessToken: string, begin_date: string, end_date: string, ignoreError: WxIsv.IgnoreError = []) {
+    return await this.request('POST', '/datacube/getweanalysisappiddailysummarytrend', { begin_date, end_date }, { access_token: accessToken }, ignoreError) as WxIsv.WxIsvStatDailySummary
   }
 
   // 获取用户访问小程序数据日趋势
-  async getDailyVisitTrend (accessToken: string, begin_date: string, end_date: string) {
-    return await this.request('POST', '/datacube/getweanalysisappiddailyvisittrend', { begin_date, end_date }, { access_token: accessToken }) as WxIsv.WxIsvStatVisitTrend
+  async getDailyVisitTrend (accessToken: string, begin_date: string, end_date: string, ignoreError: WxIsv.IgnoreError = []) {
+    return await this.request('POST', '/datacube/getweanalysisappiddailyvisittrend', { begin_date, end_date }, { access_token: accessToken }, ignoreError) as WxIsv.WxIsvStatVisitTrend
   }
 
   // 获取用户访问小程序日留存
-  async getDailyRetain (accessToken: string, begin_date: string, end_date: string) {
-    return await this.request('POST', '/datacube/getweanalysisappiddailyretaininfo', { begin_date, end_date }, { access_token: accessToken }) as WxIsv.WxIsvStatDailyRetain
+  async getDailyRetain (accessToken: string, begin_date: string, end_date: string, ignoreError: WxIsv.IgnoreError = []) {
+    return await this.request('POST', '/datacube/getweanalysisappiddailyretaininfo', { begin_date, end_date }, { access_token: accessToken }, ignoreError) as WxIsv.WxIsvStatDailyRetain
   }
 
   // 获取用户画像
-  async userPortrait (accessToken: string, granularity: 'day' | 'week' | 'month', begin_date: string, end_date: string) {
-    return await this.request('POST', '/datacube/getweanalysisappiduserportrait', { begin_date, end_date }, { access_token: accessToken })
+  async userPortrait (accessToken: string, granularity: 'day' | 'week' | 'month', begin_date: string, end_date: string, ignoreError: WxIsv.IgnoreError = []) {
+    return await this.request('POST', '/datacube/getweanalysisappiduserportrait', { begin_date, end_date }, { access_token: accessToken }, ignoreError)
   }
 
   // 获取访问分布
-  async visitDistribution (accessToken: string, begin_date: string, end_date: string) {
-    return await this.request('POST', '/datacube/getweanalysisappidvisitdistribution', { begin_date, end_date }, { access_token: accessToken })
+  async visitDistribution (accessToken: string, begin_date: string, end_date: string, ignoreError: WxIsv.IgnoreError = []) {
+    return await this.request('POST', '/datacube/getweanalysisappidvisitdistribution', { begin_date, end_date }, { access_token: accessToken }, ignoreError)
 
   }
 
