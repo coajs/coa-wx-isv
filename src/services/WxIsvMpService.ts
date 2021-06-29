68a355051ee82dfb95d508088ba5462eed911e1f
Aex <spryti@qq.com>
2021-06-29 11:13:54 +0000

    Fix updateShowItem method error

--- src/services/WxIsvMpService.ts
+++ src/services/WxIsvMpService.ts
@@ -29,14 +29,14 @@ export class WxIsvMpService extends WxIsvServiceBase {
    * 使用本接口可以设置扫码关注组件所展示的公众号信息
    * 详见 https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/2.0/api/subscribe_component/updateshowwxaitem.html
    * @param accessToken
-   * @param appId 新的公众号 appid
+   * @param mpAppId 新的公众号 appid
    * @param subscribe 是否打开扫码关注组件，0 关闭，1 开启，默认为1
    */
-  async getList(accessToken: string, appId: string, subscribe = 1) {
+  async updateShowItem(accessToken: string, mpAppId: string, subscribe = 1) {
     return (await this.request(
       'POST',
       '/wxa/updateshowwxaitem',
-      { appid: appId, wxa_subscribe_biz_flag: subscribe },
+      { appid: mpAppId, wxa_subscribe_biz_flag: subscribe },
       { access_token: accessToken }
     )) as WxIsv.WxIsvNormalResponse
   }
