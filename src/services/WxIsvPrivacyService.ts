10dced9a6842b4554bf5396bd162e91022f7859c
hopegao <101623935+hopegao@users.noreply.github.com>
2022-05-16 16:02:28 +0800

    新增申请隐私接口 (#8)
    * 新增申请隐私接口
    
    * 优化代码
    
    * 优化代码

--- src/services/WxIsvPrivacyService.ts
+++ src/services/WxIsvPrivacyService.ts
@@ -1,6 +1,5 @@
 import { $ } from 'coa-helper'
 import { WxIsvServiceBase } from '../libs/WxIsvServiceBase'
-import { WxIsvPrivacySettingResponse } from '../types/privice'
 import { WxIsv } from '../typings'
 
 interface OwnerSetting {
@@ -16,6 +15,54 @@ interface OwnerSetting {
 interface SettingItem {
   privacy_key: string
   privacy_text: string
+}
+
+interface WxIsvPrivacySettingResponse {
+  errcode: number
+  errmsg: string
+  code_exist: number
+  privacy_list: string[]
+  setting_list: {
+    privacy_key: string
+    privacy_text: string
+    privacy_label: string
+  }[]
+  update_time: number
+  owner_setting: {
+    contact_phone: string
+    contact_email: string
+    contact_qq: string
+    contact_weixin: string
+    store_expire_timestamp: string
+    ext_file_media_id: string
+    notice_method: string
+  }
+  privacy_desc: {
+    privacy_desc_list: {
+      privacy_key: string
+      privacy_desc: string
+    }[]
+  }
+}
+
+interface WxIsvPrivacyInterfaceResponse {
+  errcode: number
+  errmsg: string
+  interface_list: {
+    api_name: string
+    api_ch_name: string
+    api_desc: string
+    status: number
+    api_link: string
+    group_name: string
+    apply_time?: undefined
+    audit_id?: undefined
+    fail_reason?: undefined
+  }[]
+}
+
+interface WxIsvApplyPrivacyInterfaceResponse extends WxIsv.WxIsvResponse {
+  audit_id: number
 }
 
 type PrivacyVersion = 1 | 2
@@ -63,5 +110,51 @@ export class WxIsvPrivacyService extends WxIsvServiceBase {
       { access_token: accessToken }
     )
     return res as WxIsvPrivacySettingResponse
+  }
+
+  /**
+   * 获取申请隐私接口列表
+   * https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/2.0/api/apply_api/get_privacy_interface.html
+   * @param accessToken
+   * @returns
+   */
+  async getPrivacyInterface(
+    accessToken: string
+  ) {
+    const res = await this.request(
+      'GET',
+      '/wxa/security/get_privacy_interface',
+      {},
+      { access_token: accessToken }
+    )
+    return res as WxIsvPrivacyInterfaceResponse
+  }
+
+  /**
+   * 申请隐私接口
+   * https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/2.0/api/apply_api/apply_privacy_interface.html
+   * @param accessToken
+   * @param api_name 申请的api英文名，例如wx.choosePoi，严格区分大小写
+   * @param content 申请说原因，不超过300个字符；需要以utf-8编码提交，否则会出现审核失败
+   * @param url_list (辅助网页)例如，上传官网网页链接用于辅助审核
+   * @param pic_list (辅助图片)填写图片的url，最多10个
+   * @param video_list (辅助视频)填写视频的链接，最多支持1个；视频格式只支持mp4格式
+   * @returns
+   */
+  async applyPrivacyInterface(
+    accessToken: string,
+    api_name: string,
+    content: string,
+    url_list: string[],
+    pic_list: string[],
+    video_list: string[]
+  ) {
+    const res = await this.request(
+      'POST',
+      '/wxa/security/apply_privacy_interface',
+      { api_name, content, url_list, pic_list, video_list },
+      { access_token: accessToken }
+    )
+    return res as WxIsvApplyPrivacyInterfaceResponse
   }
 }
