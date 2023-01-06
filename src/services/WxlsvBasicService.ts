4071a46980ae00e5b08d1fc8f5de508b2985ca74
ityifan <329686198@qq.com>
2023-01-06 15:46:51 +0800

    修改传入参数类型

--- src/services/WxlsvBasicService.ts
+++ src/services/WxlsvBasicService.ts
@@ -3,7 +3,7 @@ import { WxIsv } from '../typings'
 
 export class WxIsvBasicservice extends WxIsvServiceBase {
     // 设置订单页 path 信息
-    async applySetOrderPathInfo(accessToken: string, batchReq: Record<string, string>) {
+    async applySetOrderPathInfo(accessToken: string, batchReq: Record<any, any>) {
         return (await this.request(
             'POST',
             '/wxa/security/applysetorderpathinfo',
